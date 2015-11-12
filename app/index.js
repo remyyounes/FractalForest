0289420f843397eab3851b8420caad0bcaed2d7e
remyyounes <ryounes@gmail.com>
2015-11-11 23:11:05 -0800

    damping

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const P = Physijs(THREE);
 P.scripts.worker = 'libs/physi-worker.js';
 P.scripts.ammo = 'ammo.js';
 
-let pos = 0;
 let renderer = null;
 let scene = null;
 let light = null;
@@ -14,16 +13,18 @@ let trunk = null;
 let ground = null;
 let camera = null;
 let springLine = null;
+let velocityLine = null;
+let dampLine = null;
 
 function initBoxes() {
   const boxMaterial = P.createMaterial(
-    new THREE.MeshBasicMaterial({ color: 0x00FF00 }),
+    new THREE.MeshBasicMaterial({ color: 0x00FF00, transparent: true, opacity: 0.4 }),
     0, // friction
     32.8 // restitution/bounciness
   );
 
   box = new P.BoxMesh(
-    new THREE.CubeGeometry( 2, 2, 2 ),
+    new THREE.CubeGeometry( 1, 1, 1 ),
     boxMaterial,
   );
 
@@ -89,8 +90,8 @@ function initCamera() {
     1,
     1000
   );
-  camera.position.set( 150, 70, 60 );
-  camera.lookAt( new THREE.Vector3(0, -10, 0));
+  camera.position.set( 50, 30, 60 );
+  camera.lookAt( new THREE.Vector3(0, 5, 0));
   scene.add( camera );
 }
 
@@ -106,12 +107,24 @@ function initScene() {
 }
 
 function initVectors() {
-  const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+  const material = new THREE.LineBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.5 });
+  const materialRed = new THREE.LineBasicMaterial({ color: 0xff0000, transparent: true, opacity: 1.5 });
+  const materialYellow = new THREE.LineBasicMaterial({ color: 0xffff00, transparent: true, opacity: 1.5 });
   const geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(30, -110, 0));
-  geometry.vertices.push(new THREE.Vector3(30, 110, 0));
+  const velocityGeometry = new THREE.Geometry();
+  const dampGeometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  velocityGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  velocityGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  dampGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  dampGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
   springLine = new THREE.Line(geometry, material);
+  velocityLine = new THREE.Line(velocityGeometry, materialRed);
+  dampLine = new THREE.Line(dampGeometry, materialYellow);
   scene.add(springLine);
+  scene.add(velocityLine);
+  scene.add(dampLine);
 }
 
 function fixBranch() {
@@ -119,22 +132,34 @@ function fixBranch() {
   const target = box.position.clone().sub( anchor );
 
   const distance = target.length();
-  const v = box.getLinearVelocity().length();
-  const k = 1;
-  const b = 1;
-  const vb = (box.position.y < anchor.y ) ? -v * b : v * b;
+  const velocity = box.getLinearVelocity();
+  const v = velocity.length();
+  const k = 200;
+  const b = 10;
+  // const vb = velocity.y < 0 ? -v * b : v * b;
+  // const vb = v * b;
+  // const vb = velocity.length() * b;
+  const vb = v * b;
   const kd = k * distance;
-  const force = kd - vb;
-  const finalForce = target.clone().multiplyScalar(-force);
+  const force = -kd;
+  const finalForce = target.clone().normalize().multiplyScalar(force);
+  const dampForce = velocity.clone().normalize().multiplyScalar(-vb);
+  console.log(force);
 
 
+  velocityLine.geometry.vertices[0] = box.position.clone();
+  velocityLine.geometry.vertices[1] = box.position.clone().add(box.getLinearVelocity().clone());
   springLine.geometry.vertices[0] = box.position.clone();
-  // springLine.geometry.vertices[1] = box.position.clone().add(finalForce);//anchor.clone(); //target.clone();
-  springLine.geometry.vertices[1] = box.position.clone().sub(target.clone());
+  springLine.geometry.vertices[1] = box.position.clone().add(finalForce);
+  dampLine.geometry.vertices[0] = box.position.clone();
+  dampLine.geometry.vertices[1] = box.position.clone().add(dampForce);
 
+  velocityLine.geometry.verticesNeedUpdate = true;
   springLine.geometry.verticesNeedUpdate = true;
+  dampLine.geometry.verticesNeedUpdate = true;
 
   box.applyCentralForce(finalForce);
+  box.applyCentralForce(dampForce);
 }
 
 function render() {
